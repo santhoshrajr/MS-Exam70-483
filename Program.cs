ae016f203e01a1927bf084a9eb2511cd1647a1ad
santhoshrajr <sravira@g.clemson.edu>
2019-03-10 18:23:49 -0400

    Task parallel library methods

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using MS_Exam70_483.MultiThreading_Async.TaskParallelLibrary;
+
+namespace MS_Exam70_483
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TPLManageParallel();
+        }
+        public static void TPLInvoke() 
+        {
+             ParallelInvoke.Process();
+        }
+
+        public static void TPLForEach() 
+        { 
+            ParallelForEach.Process();
+        }
+        public static void TPLFor()
+        {
+            ParallelFor.Process();
+        }
+        public static void TPLManageParallel()
+        {
+            ManageParallelLoop.Process();
+        }
+    }
+}
