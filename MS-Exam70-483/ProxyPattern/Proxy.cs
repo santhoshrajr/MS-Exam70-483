06ebc6f166f815e5aa9409f48ea02b5d78bdfd19
santhoshrajr <sravira@g.clemson.edu>
2019-03-18 23:06:49 -0400

    Proxy Pattern

---
+++ MS-Exam70-483/ProxyPattern/Proxy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MS_Exam70_483.ProxyPattern
+{
+    //ISubject
+    public interface IBookParser
+    {
+        int GetNumberOfPages();
+    }
+    //Real Subject
+    public class BookParser : IBookParser
+    {
+        public BookParser(string book)
+        {
+            //Expensive Operation.
+        }
+        public int GetNumberOfPages()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    //proxy Following Same interface of ISubject
+    public class LazyBookParserProxy : IBookParser
+    {
+        private string _book;
+        private BookParser bookParser = null;
+        public LazyBookParserProxy(string book)
+        {
+            // Delaying the instantiation of the Actual book Parser
+            _book = book;
+        }
+        public int GetNumberOfPages()
+        {
+            //Constructing book parse only when the method is invoked
+            if(bookParser == null)
+            {
+                this.bookParser = new BookParser(_book);
+            }
+            return this.bookParser.GetNumberOfPages();
+            throw new NotImplementedException();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -9,3 +9,4 @@ This is an ongoing effort in my challenge of <b> 100DaysOfCode</b>
 # Day6 : Dependency Injection
 # Day7 : Adapter Pattern
 # Day8 : Facade Pattern
+# Day9 : Proxy Pattern
